6ae27f437a6d4eb4731519b3b15f9c5c9607790d
kadiace <rlacjfghks95@gmail.com>
2026-04-01 11:04:21 +0900

    Restructure scripts.

--- Assets/Scripts/TrashTitanCoopControlGuide.cs
+++
@@ -1,7 +0,0 @@
-namespace TrashMan
-{
-    public static class TrashTitanCoopControlGuide
-    {
-        public const string Controls = "1=Body, 2=LeftArm, 3=RightArm, 4=LeftLeg, 5=RightLeg";
-    }
-}
