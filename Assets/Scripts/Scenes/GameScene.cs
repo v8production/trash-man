33e1530802167a4b631eb0e347d40b614b194bed
kadiace <rlacjfghks95@gmail.com>
2026-05-08 13:15:33 +0900

    Simplify load titan, grolar.

--- Assets/Scripts/Scenes/GameScene.cs
+++ Assets/Scripts/Scenes/GameScene.cs
@@ -5,109 +5,42 @@ public class GameScene : BaseScene
 {
     private const string TitanPrefabName = "Titan";
     private const string GrolarPrefabName = "Grolar";
+    private static readonly Vector3 TitanSpawnPosition = new(0, 1.5f, 0);
+    private static readonly Quaternion TitanSpawnRotation = Quaternion.Euler(0f, 0f, 0f);
     private static readonly Vector3 GrolarSpawnPosition = new(0, 0, 1);
     private static readonly Quaternion GrolarSpawnRotation = Quaternion.Euler(0f, 180f, 0f);
     private TitanController _titanController;
-    private TitanRoleNetworkDriver _titanRoleDriver;
     private GrolarController _grolarController;
 
     private UI_Boss _bossUi;
     private UI_TitanStat _titanStatUi;
     private UI_GameMenu _gameMenuUi;
 
-    private static void LoadManagers()
-    {
-        _ = Managers.Input;
-        _ = Managers.TitanRole;
-    }
-
     private void EnsureTitanRuntime()
     {
-        _titanController = FindAnyObjectByType<TitanController>();
+        GameObject titanObject = Managers.Resource.Instantiate(TitanPrefabName);
+        titanObject.transform.SetPositionAndRotation(TitanSpawnPosition, TitanSpawnRotation);
 
-        if (_titanController == null)
-        {
-            // Prefer using an authored Titan already placed in the scene.
-            GameObject titanObject = GameObject.Find(TitanPrefabName);
-            if (titanObject == null)
-                titanObject = Managers.Resource.Instantiate(TitanPrefabName);
-            if (titanObject == null)
-                return;
-
-            // If we instantiated a new titan at the origin, spawn it slightly above the floor.
-            if (titanObject.scene.IsValid() && titanObject.transform.position.y <= 0.01f)
-                titanObject.transform.position = new Vector3(titanObject.transform.position.x, 1.5f, titanObject.transform.position.z);
-
-            _titanController = titanObject.GetComponent<TitanController>();
-            if (_titanController == null)
-            {
-                Debug.LogError($"{InputDebug.Prefix} Titan prefab is missing TitanController. Add it to Resources/Prefabs/Titan.prefab");
-                return;
-            }
-        }
-
-        _titanController.EnsureInitialized();
-
-        _titanRoleDriver = _titanController.GetComponent<TitanRoleNetworkDriver>();
-        if (_titanRoleDriver == null)
-        {
-            Debug.LogError($"{InputDebug.Prefix} Titan prefab is missing TitanRoleNetworkDriver. Add it to Resources/Prefabs/Titan.prefab");
-            return;
-        }
+        _titanController = titanObject.GetComponent<TitanController>();
     }
 
     private void EnsureGrolarRuntime()
     {
-        _grolarController = FindAnyObjectByType<GrolarController>();
-        if (_grolarController != null)
-            return;
-
-        GameObject grolarObject = GameObject.Find(GrolarPrefabName);
-        if (grolarObject == null)
-            grolarObject = Managers.Resource.Instantiate(GrolarPrefabName);
-
-        if (grolarObject == null)
-        {
-            Debug.LogError($"{InputDebug.Prefix} Failed to load Grolar prefab from Resources/Prefabs/Grolar.prefab");
-            return;
-        }
-
+        GameObject grolarObject = Managers.Resource.Instantiate(GrolarPrefabName);
         grolarObject.transform.SetPositionAndRotation(GrolarSpawnPosition, GrolarSpawnRotation);
-
         _grolarController = grolarObject.GetComponent<GrolarController>();
     }
 
-    private void EnsureBossUI()
+    private void EnsureUI()
     {
-        if (_bossUi != null)
-            return;
-
         _bossUi = Managers.UI.ShowSceneUI<UI_Boss>(nameof(UI_Boss));
-    }
-
-    private void EnsureTitanStatUI()
-    {
-        if (_titanStatUi != null)
-            return;
-
         _titanStatUi = Managers.UI.ShowSceneUI<UI_TitanStat>(nameof(UI_TitanStat));
-    }
-
-    private void EnsureGameMenuUI()
-    {
-        if (_gameMenuUi != null)
-            return;
-
         _gameMenuUi = Managers.UI.ShowSceneUI<UI_GameMenu>(nameof(UI_GameMenu));
-        if (_gameMenuUi != null)
-            _gameMenuUi.gameObject.SetActive(false);
+        _gameMenuUi.gameObject.SetActive(false);
     }
 
     private void MapStatsToUIs()
     {
-        EnsureBossUI();
-        EnsureTitanStatUI();
-
         if (_bossUi != null && _grolarController != null)
             _bossUi.SetStat(_grolarController.GetComponent<BossStat>());
 
@@ -122,12 +55,9 @@ public class GameScene : BaseScene
 
         Debug.Log($"{InputDebug.Prefix} GameScene.Init SceneType={SceneType}");
 
-        LoadManagers();
         EnsureTitanRuntime();
         EnsureGrolarRuntime();
-        EnsureBossUI();
-        EnsureTitanStatUI();
-        EnsureGameMenuUI();
+        EnsureUI();
         MapStatsToUIs();
         CleanupLobbyRangers();
         Managers.Input.SetMode(Define.InputMode.Player);
@@ -143,10 +73,6 @@ public class GameScene : BaseScene
 
     private void ToggleGameMenu()
     {
-        EnsureGameMenuUI();
-        if (_gameMenuUi == null)
-            return;
-
         bool shouldShow = !_gameMenuUi.gameObject.activeSelf;
         _gameMenuUi.gameObject.SetActive(shouldShow);
         Managers.Input.SetMode(shouldShow ? Define.InputMode.UI : Define.InputMode.Player);
@@ -175,7 +101,6 @@ public class GameScene : BaseScene
     public override void Clear()
     {
         _titanController = null;
-        _titanRoleDriver = null;
         _grolarController = null;
         _bossUi = null;
         _titanStatUi = null;
