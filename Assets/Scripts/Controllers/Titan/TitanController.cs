d33e79c5ba8b2123837a703cea3efbf39bc86001
kadiace <rlacjfghks95@gmail.com>
2026-06-10 22:40:26 +0900

    Add shield effect.

--- Assets/Scripts/Controllers/Titan/TitanController.cs
+++ Assets/Scripts/Controllers/Titan/TitanController.cs
@@ -15,14 +15,29 @@ public class TitanController : MonoBehaviour
     private TitanDrillController leftDrillController;
     private TitanClawWireController rightClawWireController;
 
+    [Header("Shield Visual")]
+    [SerializeField] private string shieldPrefabName = "Shield";
+    [SerializeField] private Vector3 shieldLocalPosition = new(0f, 0.5f, 0f);
+    [SerializeField] private Vector3 shieldLocalEulerAngles;
+    [SerializeField] private Vector3 shieldLocalScale = new(1f, 1.2f, 1f);
+
     TitanStat _stat;
     public TitanStat Stat { get { return _stat; } }
 
     bool _guard;
     bool _leftDrillActive;
     int _rightClawLaunchCount;
+    GameObject shieldVisual;
 
-    public bool Guard { get { return _guard; } set { _guard = value; } }
+    public bool Guard
+    {
+        get { return _guard; }
+        set
+        {
+            _guard = value;
+            SetShieldVisualActive(_guard);
+        }
+    }
     public bool LeftDrillActive { get { return _leftDrillActive; } set { _leftDrillActive = value; } }
     public int RightClawLaunchCount { get { return _rightClawLaunchCount; } }
     public TitanClawWireController RightClawWire => rightClawWireController;
@@ -54,5 +69,28 @@ public class TitanController : MonoBehaviour
         rightLegController = gameObject.GetOrAddComponent<TitanRightLegRoleController>();
         leftDrillController = gameObject.GetOrAddComponent<TitanDrillController>();
         rightClawWireController = gameObject.GetOrAddComponent<TitanClawWireController>();
+        SetShieldVisualActive(_guard);
+    }
+
+    private void SetShieldVisualActive(bool active)
+    {
+        if (active)
+        {
+            if (shieldVisual == null)
+            {
+                shieldVisual = Managers.Resource.Instantiate(shieldPrefabName, transform);
+                shieldVisual.transform.localPosition = shieldLocalPosition;
+                shieldVisual.transform.localRotation = Quaternion.Euler(shieldLocalEulerAngles);
+                shieldVisual.transform.localScale = shieldLocalScale;
+            }
+
+            return;
+        }
+
+        if (shieldVisual != null)
+        {
+            Managers.Resource.Destory(shieldVisual);
+            shieldVisual = null;
+        }
     }
 }
